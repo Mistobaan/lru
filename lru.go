45bf14ee91703abfd1d746299573a63f495b8e6a
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2014-09-25 09:14:07 -0700

    update lru logic

--- lru.go
+++ lru.go
@@ -33,65 +33,100 @@ type Cache struct {
 	size     int64
 	capacity int64
 
+	defaultExpiration time.Duration
+
 	// don't thrash the allocated items
 	pool *Item
 }
 
 // NewCache creates a cache that will keep only the last `size` element in memory. As mapper it uses a standard map[Key]*item
+// The default expiration  is 60 second
 func NewCache(capacity int64) *Cache {
 	return &Cache{
-		head:     nil,
-		tail:     nil,
-		size:     0,
-		capacity: capacity,
-		pool:     nil,
+		head:              nil,
+		tail:              nil,
+		table:             make(map[string]*Item),
+		size:              0,
+		capacity:          capacity,
+		defaultExpiration: 60 * time.Second,
+		pool:              nil,
 	}
 }
 
-func (c *Cache) pushFront(it *Item) {
+// Set sets a value in the cache.
+func (c *Cache) Set(key string, value []byte) {
+	c.SetExpire(key, value, c.defaultExpiration)
+}
+
+// SetExpire sets a key and when it would expire
+func (c *Cache) SetExpire(key string, value []byte, expiration time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	item, ok := c.table[key]
+	if !ok {
+		c.addNew(key, value, expiration)
+	} else {
+		c.updateInPlace(item, key, value, expiration)
+	}
+}
+
+// Get Gets the latest value of key if available.
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	item, ok := c.table[key]
+	if !ok {
+		return nil, false
+	}
+
+	if time.Now().After(item.lastAccess.Add(item.expiresIn)) {
+		c.deleteKey(key)
+		return nil, false
+	}
+
+	item.lastAccess = time.Now()
+
+	c.pushFront(item)
+	return item.value, true
+}
+
+func (c *Cache) pushFront(item *Item) {
 	// assuming item != null
-	if it == c.head {
+	if item == c.head {
 		// item already in front
 		return
-	} else if it == c.tail {
-		c.tail = it.prev
-		c.tail.next = nil
-	} else {
-		// A -> B -> C: remove B
-		A := it.prev
-		C := it.next
-		A.next = C
-		C.prev = A
 	}
-	it.next = c.head
-	c.head.prev = it
-	it.prev = nil
-	c.head = it
+
+	c.pop(item)
+
+	item.next = c.head
+	c.head.prev = item
+	item.prev = nil
+	c.head = item
 }
 
-func (c *Cache) popTail() {
-	tail := c.tail
-	it := tail
-
-	// tail == c.head then is the only item
-	if c.head == tail {
-		c.head = nil
-		c.tail = nil
+// pop removes the item from the list
+func (c *Cache) pop(it *Item) {
+	// A -> it -> C: remove B
+	if it == c.head {
+		c.head = it.next
+	}
+	if it == c.tail {
+		c.tail = it.prev
 	}
 
-	// there is at least one element
-	// head -> a
-	//         |
-	// tail -> b
-	if tail != nil {
-		// a -> tail
-		a := tail.prev
-		if a != nil {
-			a.next = nil
-		}
-		c.tail = a
+	A := it.prev
+	C := it.next
+	if A != nil {
+		A.next = C
 	}
-	c.delItem(it)
+	if C != nil {
+		C.prev = A
+	}
+	it.next = nil
+	it.prev = nil
 }
 
 func (c *Cache) newItem(key string, value []byte) *Item {
@@ -109,68 +144,59 @@ func (c *Cache) newItem(key string, value []byte) *Item {
 	c.pool = r.next
 	r.key = key
 	r.value = value
+	r.next = nil
+	r.prev = nil
 	return r
 }
 
-func (c *Cache) delItem(it *Item) {
+func (c *Cache) deleteItem(item *Item) {
 	a := c.pool
-	c.pool = it
-	it.next = a
-	it.prev = nil
-	it.value = nil
-	it.expiresIn = 0
-	it.key = ""
+	c.pool = item
+	item.next = a
+	item.prev = nil
+	item.value = nil
+	item.expiresIn = 0
+	item.key = ""
 }
 
-func (c *Cache) pop(it *Item) {
-	if it == c.tail {
-		c.popTail()
-	} else if it == c.head {
-		// Head -> it ->  a
-		c.head = it.next
-		if c.head != nil {
-			c.head.prev = nil
-		}
-		c.delItem(it)
-	} else {
-		// A -> B -> C: remove B
-		A := it.prev
-		C := it.next
-		A.next = C
-		C.prev = A
-		c.delItem(it)
-	}
-}
-
-// Set sets a value in the cache.
-func (c *Cache) Set(key string, value []byte) {
-
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	if c.capacity == 0 {
-		return
-	}
-
+// internal delete only. Use with locks
+func (c *Cache) deleteKey(key string) {
 	item, ok := c.table[key]
 	if !ok {
-		c.addNew(key, value)
-	} else {
-		c.updateInPlace(item, key, value)
+		return
 	}
+	c.pop(item)
+	c.deleteItem(item)
+	delete(c.table, key)
 }
 
-func (c *Cache) addNew(key string, value []byte) {
+func (c *Cache) addNew(key string, value []byte, expiration time.Duration) {
 	item := c.newItem(key, value)
+
+	if c.head == nil {
+		c.head = item
+		c.tail = item
+	} else {
+		item.next = c.head
+		c.head = item
+	}
+
+	item.expiresIn = expiration
+	item.lastAccess = time.Now()
+
 	c.table[key] = item
-	c.pushFront(item)
+
 	c.checkCapacity()
 }
 
-func (c *Cache) updateInPlace(item *Item, key string, value []byte) {
+func (c *Cache) updateInPlace(item *Item, key string, value []byte, expiration time.Duration) {
 	valueSize := int64(cap(value))
 	sizeDiff := valueSize - int64(cap(item.value))
 	item.value = value
+
+	item.lastAccess = time.Now()
+	item.expiresIn = expiration
+
 	c.size += sizeDiff
 	c.pushFront(item)
 	c.checkCapacity()
@@ -179,50 +205,16 @@ func (c *Cache) updateInPlace(item *Item, key string, value []byte) {
 func (c *Cache) checkCapacity() {
 	for c.size > c.capacity {
 		item := c.tail
-		c.delete(item.key)
+		c.deleteKey(item.key)
 		c.size -= int64(cap(item.value))
 	}
-}
-
-// Get Gets the latest value of key if available.
-func (c *Cache) Get(key string) ([]byte, bool) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	if c.capacity == 0 {
-		return nil, false
-	}
-
-	item, ok := c.table[key]
-	if !ok {
-		return nil, false
-	}
-
-	if time.Now().After(item.lastAccess.Add(item.expiresIn)) {
-		c.delete(key)
-		return nil, false
-	}
-
-	c.pushFront(item)
-	return item.value, true
 }
 
 // Delete Deletes a key from the cache. no action is taken if the key is not found .
 func (c *Cache) Delete(key string) {
 	c.mu.Lock()
-	c.delete(key)
+	c.deleteKey(key)
 	c.mu.Unlock()
-}
-
-// internal delete only. Use with locks
-func (c *Cache) delete(key string) {
-	it, ok := c.table[key]
-	if !ok {
-		return
-	}
-	c.pop(it)
-	delete(c.table, key)
-
 }
 
 // Size returns the number of items in the cache
--- lru_test.go
+++ lru_test.go
@@ -1,11 +1,13 @@
 package lru
 
 import (
+	"bytes"
+	"strconv"
 	"testing"
 )
 
 func TestGetEmpty(t *testing.T) {
-	cache := New(1)
+	cache := NewCache(1024 * 10)
 	_, ok := cache.Get("invalid")
 	if ok {
 		t.Error("expected false")
@@ -13,51 +15,63 @@ func TestGetEmpty(t *testing.T) {
 }
 
 func TestSetEmpty(t *testing.T) {
-	cache := New(1)
-	exp := 1000
-	result := cache.Set("key", exp)
-	if result != nil {
-		t.Error("nil expected")
-	}
-	value, _ := cache.Get("key")
-	if value != exp {
-		t.Errorf("Expected %v got %v", exp, value)
+	cache := NewCache(1024 * 10)
+
+	exp := []byte{0xFF}
+	cache.Set("key", exp)
+
+	value, ok := cache.Get("key")
+
+	if !ok || !bytes.Equal(value, exp) {
+		t.Errorf("Expected %v got %v %v", exp, value, cache.table)
 	}
 }
 
 func TestSetTwiceSameKey(t *testing.T) {
-	cache := New(1)
-	cache.Set("same", 1000)
-	cache.Set("same", 2000)
+	cache := NewCache(1024 * 10)
+	exp := []byte{0xFF}
+	exp2 := []byte{0xFE}
+
+	cache.Set("same", exp)
+	cache.Set("same", exp2)
+
 	item, ok := cache.Get("same")
-	if !ok || item != 2000 {
-		t.Error("invalid should be 2000")
+
+	if !ok || !bytes.Equal(item, exp2) {
+		t.Error("invalid should be ", exp2)
 	}
 }
 
+/*
 func TestSetTwoWithOneLimit(t *testing.T) {
-	cache := New(1)
-	cache.Set("first", 1000)
-	cache.Set("second", 2000)
+	cache := NewCache(1024 * 10)
+
+	exp := []byte{0xFF}
+	exp2 := []byte{0xFE}
+
+	cache.Set("first", exp)
+	cache.Set("second", exp2)
 	item, _ := cache.Get("first")
 	if item != nil {
 		t.Error("invalid should be nil got: ")
 	}
 	item, _ = cache.Get("second")
-	if item != 2000 {
+	if item != exp2 {
 		t.Error("invalid should be 2000")
 	}
 }
 
 func TestDeleteInvalidKey(t *testing.T) {
-	cache := New(1)
+	cache := NewCache(1024 * 10)
+
 	cache.Del("invalid")
 }
 
 func TestSetAndDelete(t *testing.T) {
-	cache := New(1)
-	cache.Set("k", 0)
-	cache.Del("k")
+	cache := NewCache(1024 * 10)
+
+	cache.Set("k", []byte{})
+	cache.Delete("k")
 	if item, _ := cache.Get("k"); item != nil {
 		t.Fail()
 	}
@@ -89,7 +103,8 @@ var getTests = []struct {
 
 func TestGet(t *testing.T) {
 	for _, tt := range getTests {
-		lru := New(1)
+		cache := NewCache(1024 * 10)
+
 		lru.Set(tt.keyToAdd, 1234)
 		val, ok := lru.Get(tt.keyToGet)
 		if ok != tt.expectedOk {
@@ -101,7 +116,8 @@ func TestGet(t *testing.T) {
 }
 
 func TestRemove(t *testing.T) {
-	lru := New(1)
+	cache := NewCache(1024 * 10)
+
 	lru.Set("myKey", 1234)
 	if val, ok := lru.Get("myKey"); !ok {
 		t.Fatal("TestRemove returned no match")
@@ -114,11 +130,27 @@ func TestRemove(t *testing.T) {
 		t.Fatal("TestRemove returned a removed entry")
 	}
 }
+*/
 
 func Benchmark_Insert(b *testing.B) {
-	cache := New(1)
+	cache := NewCache(1024 * 10)
+
 	for i := 0; i < b.N; i++ {
-		cache.Set(i, i)
+		key := strconv.Itoa(i)
+		cache.Set(key, []byte{0x0, 0x0, 0x0, 0x0})
 	}
 }
 
+func Benchmark_Fetch(b *testing.B) {
+	cache := NewCache(1024 * 10)
+
+	for i := 0; i < 1000; i++ {
+		key := strconv.Itoa(i)
+		cache.Set(key, []byte{0x0, 0x0, 0x0, 0x0})
+	}
+
+	for i := 0; i < b.N; i++ {
+		key := strconv.Itoa(i % 1000)
+		cache.Get(key)
+	}
+}
