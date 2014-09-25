0e9357cbd3e4745cb9039be473d4ad56970cfd30
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2014-09-24 19:32:00 -0700

    wip

--- lru.go
+++ lru.go
@@ -1,73 +1,54 @@
-// Package lru implements an LRU cache.
+// Package lru implements a high efficient LRU cache that stores map[string][]byte and with expiration date
 package lru
 
 import (
-	"fmt"
+	"sync"
+	"time"
 )
 
-// TODO:
-// - add a perfect hash function
-// - allow to resize the cache
-
-// A Key may be any value that is comparable. See http://golang.org/ref/spec#Comparison_operators
-type Key interface{}
-
+// Item is the container for the elements inside the LRU cache
 type Item struct {
-	next  *Item
-	prev  *Item
-	key   Key
-	value interface{}
+	next *Item
+	prev *Item
+
+	key   string
+	value []byte
+
+	lastAccess time.Time
+	expiresIn  time.Duration
 }
 
+// Cache is a typical LRU cache implementation. When an element is
+// accessed it is promoted to the head of the list, and when space is
+// needed the element at the tail of the list (the least recently used
+// element) is evicted.
 type Cache struct {
-	table   Mapper
-	head     *Item
-	tail     *Item
-	free     uint
-	capacity uint
-	item_pool *Item
+	mu sync.Mutex
+
+	head *Item
+	tail *Item
+
+	table map[string]*Item
+
+	size     int64
+	capacity int64
+
+	// don't thrash the allocated items
+	pool *Item
 }
 
-type Mapper interface {
-	GetItem(Key) (*Item, bool)
-	SetItem(Key, *Item)
-	DelItem(Key)
-}
-
-// Default Mapper
-type DefaultMapper map[Key]*Item
-
-func (m DefaultMapper) GetItem(k Key) (*Item, bool) {
-	item, ok := m[k]
-	return item, ok
-}
-
-func (m DefaultMapper) SetItem(k Key, value *Item) {
-	m[k] = value
-}
-
-func (m DefaultMapper) DelItem(k Key) {
-	delete(m, k)
-}
-
-// New creates a cache that will keep only the last `size` element in memory. As mapper it uses a standard map[Key]*item
-func New(size uint) *Cache {
-	return NewWithMapper(size, &DefaultMapper{})
-}
-
-// NewWithMapper creates a cache that will keep only the last `size` element in memory and will use the provided mapper
-func NewWithMapper(size uint, mapper Mapper) *Cache {
+// NewCache creates a cache that will keep only the last `size` element in memory. As mapper it uses a standard map[Key]*item
+func NewCache(capacity int64) *Cache {
 	return &Cache{
-		table:   mapper,
 		head:     nil,
 		tail:     nil,
-		free:     size,
-		capacity: size,
-		item_pool: nil,
+		size:     0,
+		capacity: capacity,
+		pool:     nil,
 	}
 }
 
-func (c *Cache) push_front(it *Item) {
+func (c *Cache) pushFront(it *Item) {
 	// assuming item != null
 	if it == c.head {
 		// item already in front
@@ -88,7 +69,7 @@ func (c *Cache) push_front(it *Item) {
 	c.head = it
 }
 
-func (c *Cache) pop_tail() {
+func (c *Cache) popTail() {
 	tail := c.tail
 	it := tail
 
@@ -113,35 +94,37 @@ func (c *Cache) pop_tail() {
 	c.delItem(it)
 }
 
-func (c *Cache) newItem(key, value interface{}) *Item {
-	if c.item_pool == nil {
+func (c *Cache) newItem(key string, value []byte) *Item {
+	// check the pool
+	if c.pool == nil {
 		return &Item{
 			key:   key,
 			value: value,
 			next:  nil,
 			prev:  nil,
 		}
-	} else {
-		r := c.item_pool
-		c.item_pool = r.next
-		r.key = key
-		r.value = value
-		return r
 	}
+
+	r := c.pool
+	c.pool = r.next
+	r.key = key
+	r.value = value
+	return r
 }
 
 func (c *Cache) delItem(it *Item) {
-	a := c.item_pool
-	c.item_pool = it
+	a := c.pool
+	c.pool = it
 	it.next = a
 	it.prev = nil
 	it.value = nil
-	it.key = nil
+	it.expiresIn = 0
+	it.key = ""
 }
 
 func (c *Cache) pop(it *Item) {
 	if it == c.tail {
-		c.pop_tail()
+		c.popTail()
 	} else if it == c.head {
 		// Head -> it ->  a
 		c.head = it.next
@@ -160,65 +143,89 @@ func (c *Cache) pop(it *Item) {
 }
 
 // Set sets a value in the cache.
-func (c *Cache) Set(key, value interface{}) error {
-	// this does not work if capacity is zero
+func (c *Cache) Set(key string, value []byte) {
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	if c.capacity == 0 {
-		return fmt.Errorf("Can't set to a zero capacity")
+		return
 	}
 
-	if c.free < 1 {
-		c.table.DelItem(c.tail.key)
-		c.pop_tail()
-		c.free += 1
-	}
-
-	it, ok := c.table.GetItem(key)
-
+	item, ok := c.table[key]
 	if !ok {
-		it = c.newItem(key, value)
-		c.table.SetItem(key, it)
-		if c.head == nil {
-			c.head = it
-			c.tail = c.head
-		}
-		c.free -= 1
+		c.addNew(key, value)
 	} else {
-		it.value = value
+		c.updateInPlace(item, key, value)
 	}
+}
 
-	c.push_front(it)
+func (c *Cache) addNew(key string, value []byte) {
+	item := c.newItem(key, value)
+	c.table[key] = item
+	c.pushFront(item)
+	c.checkCapacity()
+}
 
-	return nil
+func (c *Cache) updateInPlace(item *Item, key string, value []byte) {
+	valueSize := int64(cap(value))
+	sizeDiff := valueSize - int64(cap(item.value))
+	item.value = value
+	c.size += sizeDiff
+	c.pushFront(item)
+	c.checkCapacity()
+}
+
+func (c *Cache) checkCapacity() {
+	for c.size > c.capacity {
+		item := c.tail
+		c.delete(item.key)
+		c.size -= int64(cap(item.value))
+	}
 }
 
 // Get Gets the latest value of key if available.
-func (c *Cache) Get(key interface{}) (interface{}, bool) {
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	if c.capacity == 0 {
 		return nil, false
 	}
 
-	item, ok := c.table.GetItem(key)
+	item, ok := c.table[key]
 	if !ok {
 		return nil, false
 	}
 
-	c.push_front(item)
+	if time.Now().After(item.lastAccess.Add(item.expiresIn)) {
+		c.delete(key)
+		return nil, false
+	}
 
+	c.pushFront(item)
 	return item.value, true
 }
 
-// Del Deletes a key from the cache. no action is taken if the key is not found .
-func (c *Cache) Del(key interface{}) {
-	it, ok := c.table.GetItem(key)
+// Delete Deletes a key from the cache. no action is taken if the key is not found .
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	c.delete(key)
+	c.mu.Unlock()
+}
+
+// internal delete only. Use with locks
+func (c *Cache) delete(key string) {
+	it, ok := c.table[key]
 	if !ok {
 		return
 	}
 	c.pop(it)
-	c.free += 1
-	c.table.DelItem(key)
+	delete(c.table, key)
+
 }
 
-// Len returns the number of items in the cache
-func (c *Cache) Len() uint {
-	return c.capacity - c.free
+// Size returns the number of items in the cache
+func (c *Cache) Size() int64 {
+	return c.size
 }
